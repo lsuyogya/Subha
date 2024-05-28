4706732e56ee7e76d140c0a6c02f3cf682c30e11
Suyogya Luitel <lsuyogya@gmail.com>
2024-05-28 10:05:17 +0545

    device switch checkpoint

--- assets/js/splideInit.js
+++ assets/js/splideInit.js
@@ -1,24 +1,42 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-    var main = new Splide( '#txtSplide', {
-      type      : 'fade',
-      rewind    : true,
-      pagination: false,
-      arrows    : false,
-    } );
-  
-  
-    var thumbnails = new Splide( '#imgSplide', {
+const imgSlidePerPage = 3;
+const marginRight = '6.67px';
+document.addEventListener('DOMContentLoaded', function () {
+  var main = new Splide('#txtSplide', {
+    type: 'fade',
+    rewind: true,
+    pagination: false,
+    perPage: 1,
+  });
+
+  var thumbnails = new Splide('#imgSplide', {
     //   fixedWidth  : 100,
     //   fixedHeight : 60,
-      gap         : 10,
-      rewind      : true,
-      pagination  : false,
-      isNavigation: true,
-      perPage: 3, 
-    } );
-  
-  
-    main.sync( thumbnails );
-    main.mount();
-    thumbnails.mount();
-  } );
+    gap: 10,
+    rewind: true,
+    pagination: false,
+    arrows: false,
+    isNavigation: true,
+    perPage: imgSlidePerPage,
+  });
+
+  main.sync(thumbnails);
+  main.mount();
+  thumbnails.mount();
+  imgSlide = document.getElementById('imgSplide');
+  slideComponents = imgSlide.querySelectorAll('.splide__slide');
+  // console.log(slideComponents);
+
+  slideWidth = 'calc(100% / imgSlidePerPage - marginRight)';
+  const firstSlideWidth = `calc(100% / ${imgSlidePerPage} - ${marginRight} + 5%)`;
+  // const otherSlideWidth = `calc(100% / ${imgSlidePerPage} - ${marginRight} - 5% / ${imgSlidePerPage})`;
+  const otherSlideWidth = `calc(100%)`;
+
+  slideComponents.forEach((slide, index) => {
+    console.log(slide);
+    if (index != 1) {
+      slide.style.width = otherSlideWidth;
+      return;
+    }
+    slide.style.width = firstSlideWidth;
+  });
+});
